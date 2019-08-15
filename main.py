3b4b7c3393aa90951eb2d78b2404c36a54d86129
wyatthoffman4392 <wyatthoffman4392@users.noreply.github.com>
2019-08-14 22:25:14 -0700

    Formatted Definitions

--- main.py
+++ main.py
@@ -7,35 +7,44 @@ data = json.load(open("dictionary.json"))
 
 # Main function
 def main():
-    print(getData())
+    output = getDefinition()
 
-# Function for getting data
-def getData():
+    # An iterator to display multiple definitions for a single word
+    if type(output) == list:
+        for item in output:
+            print("-", item)
+    # Prints words with a single definition
+    else:
+        print("-", output)
+
+# Function for getting the definition
+def getDefinition():
     while True:
         userInput = input("Input a word to define: ")
+
+        # A conditional checking for the word in the dictionary
         if userInput in data:
             return data[userInput]
+            break
         elif userInput.title() in data:
             return data[userInput.title()]
+            break
         elif userInput.upper() in data:
             return data[userInput.upper()]
+            break
+        # If the users word is a close match to a different word this line executes
         elif len(get_close_matches(userInput, data.keys())) > 0:
-            while True:
-                action = input("Did you mean %s instead? {y or n]: " % get_close_matches(userInput, data.keys())[0])
-                if (action == "y"):
-                    return data[get_close_matches(userInput, data.keys())[0]]
-                    break
-                elif (action == "n"):
-                    return ("The word doesn't exist, yet.")
-                    break
-                else:
-                    return ("Invalid entry. Please enter 'y' or 'n'")
-                    continue
-
-
-
+            action = input("Did you mean %s instead? {y or n]: " % get_close_matches(userInput, data.keys())[0])
+            # User is given the choice to select if they meant a certain word or not
+            if (action == "y"):
+                return data[get_close_matches(userInput, data.keys())[0]]
+            elif (action == "n"):
+                print("The word doesn't exist, yet.")
+            else:
+                return ("Invalid entry. Please enter 'y' or 'n'")
+        # If word isn't in the dictionary the while loop resets and they can input another word
         else:
-            print("That word is not available, please check the word is spelled correctly.")
+            print("That word is not available, please check the word is spelled correctly and try again.")
             print()
             continue
 
